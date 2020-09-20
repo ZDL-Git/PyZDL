4f396b8eb88c6377552ca0ef7de9b61dda5b2a6a
ZDL <zdl_daily@163.com>
2020-09-21 01:44:42 +0800

    fix test_main.py add post hooks.

--- test/test_main.py
+++ test/test_main.py
@@ -4,18 +4,18 @@ log.theme('dark')
 from zdl.AI.pose.extractor.openpose_extractor import OpenposeExtractor
 from zdl.AI.pose.pose.body25 import BODY25
 from zdl.utils.io.log import logger, addFileLogger, configConsoleLogger
-from zdl.utils.media.image import sysShow
+from zdl.utils.media.image import sysShow, ImageCV
 
 addFileLogger('test_main.log')
 configConsoleLogger(10)
 
 if __name__ == '__main__':
     pass
-    logger.info('start.')
     OpenposeExtractor.setModel("/Volumes/CodeYard/Software/code-type/C++/openpose/models/")
     poses, datum = OpenposeExtractor(BODY25) \
+        .addPreHooks([lambda x: print(x.shape)]) \
+        .addPostHooks([lambda x, y: ImageCV(y.cvOutputData).show()]) \
+        .addPostHooks([lambda x, y: sysShow(y.cvOutputData)]) \
         .extract('/Volumes/CodeYard/Software/code-type/Python/ZDL-AI/resources/media/mike-tyson-full-body.png',
                  silent=False)
-    # .addPreHooks(lambda x: print(x.shape))
-    sysShow(datum.cvOutputData)
     logger.info('exit.')
--- zdl/AI/pose/extractor/extractor.py
+++ zdl/AI/pose/extractor/extractor.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 from zdl.utils.io.log import logger
 
-from zdl.AI.pose.pose.pose import Pose
+from zdl.AI.pose.pose.pose import Pose, Poses
 
 
 class Extractor(ABC):
@@ -13,6 +13,7 @@ class Extractor(ABC):
 
     def __init__(self):
         self.pre_hooks = []
+        self.post_hooks = []
         self.pose_type: Type[Pose] = None
 
     @abstractmethod
@@ -24,6 +25,10 @@ class Extractor(ABC):
         # all hooks should work in-place
         for h in self.pre_hooks:
             h(img)
+
+    def _callPostHooks(self, poses: Poses, others):
+        for h in self.post_hooks:
+            h(poses, others)
 
     @classmethod
     def setModel(cls, path):
@@ -38,6 +43,15 @@ class Extractor(ABC):
             raise TypeError('hook_or_hooks type error!')
         return self
 
+    def addPostHooks(self, hook_or_hooks: Union[Callable[[np.ndarray], None], List[Callable[[np.ndarray], None]]]):
+        if isinstance(hook_or_hooks, Callable):
+            self.post_hooks.append(hook_or_hooks)
+        elif isinstance(hook_or_hooks, List):
+            self.post_hooks += hook_or_hooks
+        else:
+            raise TypeError('hook_or_hooks type error!')
+        return self
+
     def extract(self, img, silent=True):
         # entry function
         if not silent:
@@ -47,4 +61,5 @@ class Extractor(ABC):
 
         self._callPreHooks(img)
         poses, others = self._callExtractorCore(img)
+        self._callPostHooks(poses, others)
         return poses, others
