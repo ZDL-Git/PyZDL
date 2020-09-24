41ff972d132bf756e4cb20e0c306f9b244be4969
ZDL <zdl_daily@163.com>
2020-09-24 19:16:10 +0800

    update rect class.

--- zdl/utils/media/rect.py
+++ zdl/utils/media/rect.py
@@ -5,13 +5,22 @@ class Rect:
     def __init__(self, xyxy=None, yxyx=None, xywh=None):
         if xyxy is not None:
             self.xyxy = np.asarray(xyxy)
-            self.w, self.h = self.xyxy[[2, 3]] - self.xyxy[[0, 1]]
+            self.yxyx = self.xyxy[[1, 0, 3, 2]]
+            self.xywh = self.xyxy - [0, 0, self.xyxy[0], self.xyxy[1]]
+            self.w, self.h = self.xywh[-2:]
         elif yxyx is not None:
-            self.xyxy = np.asarray(yxyx)[[1, 0, 3, 2]]
-            self.w, self.h = self.xyxy[[2, 3]] - self.xyxy[[0, 1]]
+            self.yxyx = np.asarray(yxyx)
+            self.xyxy = self.yxyx[[1, 0, 3, 2]]
+            self.xywh = self.xyxy - [0, 0, self.xyxy[0], self.xyxy[1]]
+            self.w, self.h = self.xywh[-2:]
         elif xywh is not None:
-            self.xyxy = np.asarray(xywh) + [0, 0, xywh[0], xywh[1]]
+            self.xywh = np.asarray(xywh)
+            self.xyxy = self.xywh + [0, 0, xywh[0], xywh[1]]
+            self.yxyx = self.xyxy[[1, 0, 3, 2]]
             self.w, self.h = xywh[2], xywh[3]
         else:
             raise ValueError('xyxy/yxyx/xywh, one of them must have a value!')
         self.c_l, self.r_t, self.c_r, self.r_b = self.xyxy
+
+    def toInt(self) -> 'Rect':
+        return self.__class__(xyxy=self.xyxy.astype(int))
