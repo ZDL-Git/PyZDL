c4b7f7e3fec3479af6c69915456958f1d1764921
ZDL <zdl_daily@163.com>
2020-09-24 17:33:53 +0800

    fix.

--- zdl/utils/media/rect.py
+++ zdl/utils/media/rect.py
@@ -3,16 +3,15 @@ import numpy as np
 
 class Rect:
     def __init__(self, xyxy=None, yxyx=None, xywh=None):
-        if xyxy:
+        if xyxy is not None:
             self.xyxy = np.asarray(xyxy)
             self.w, self.h = self.xyxy[[2, 3]] - self.xyxy[[0, 1]]
-        elif yxyx:
+        elif yxyx is not None:
             self.xyxy = np.asarray(yxyx)[[1, 0, 3, 2]]
             self.w, self.h = self.xyxy[[2, 3]] - self.xyxy[[0, 1]]
-        elif xywh:
+        elif xywh is not None:
             self.xyxy = np.asarray(xywh) + [0, 0, xywh[0], xywh[1]]
             self.w, self.h = xywh[2], xywh[3]
         else:
             raise ValueError('xyxy/yxyx/xywh, one of them must have a value!')
         self.c_l, self.r_t, self.c_r, self.r_b = self.xyxy
-
