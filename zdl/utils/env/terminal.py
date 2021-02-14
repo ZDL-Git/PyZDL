2a7e0bc03f95343640b7a9d6c31ece9ebf7de316
ZDL <zdl_daily@163.com>
2021-02-14 17:24:12 +0800

    fix checkOutput method.

--- zdl/utils/env/terminal.py
+++ zdl/utils/env/terminal.py
@@ -6,16 +6,15 @@ from zdl.utils.io.log import logger
 class Terminal:
     @classmethod
     def checkCall(cls, command):
-        """执行命令，打印结果和状态，执行错误则抛出异常
+        """执行命令，打印结果和状态。执行错误抛出异常
         """
         logger.debug(command)
         subprocess.check_call(command, shell=True)
 
     @classmethod
     def checkOutput(cls, command):
-        """执行命令，返回运行结果，而不是打印
+        """执行命令，返回运行结果，不打印。执行错误抛出异常
         """
         logger.debug(command)
         output = subprocess.check_output(command, shell=True)
-        logger.debug(output)
-        return output
+        return output.decode("utf-8")
